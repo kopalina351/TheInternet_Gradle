9ec23d37a4366717604390c6ec00b4b43d8029d0
Alina_Kopylova <Alina_Kopylova@epam.com>
2021-01-28 20:09:16 +0200

    added packages

--- src/main/java/PagesSteps/IFramePageSteps.java
+++ src/main/java/PagesSteps/IFramePageSteps.java
@@ -6,7 +6,7 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.switchTo;
 
-public class IFramePageSteps extends IframePage {
+public class IFramePageSteps {
     IframePage iframePage = new IframePage();
 
     public void openIframePageAndSwitchToFrame(String urlName, SelenideElement nameFrame){
