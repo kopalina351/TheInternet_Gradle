bc5e2877c627aae4fbfe8c4ac867133dd65231b6
Alina Kopylova <kopalina351@gmail.com>
2021-02-17 21:03:54 +0200

    added dataProvider JUnit

--- src/test/java/FramesTests.java
+++ src/test/java/FramesTests.java
@@ -1,15 +1,28 @@
 import PagesSteps.IFramePageSteps;
+import com.codeborne.selenide.Selenide;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-
+@RunWith(DataProviderRunner.class)
 public class FramesTests {
-
-    private final String newText = "Testing iFrame";
 
     IFramePageSteps iFramePageSteps = new IFramePageSteps();
 
+    @DataProvider
+    public static Object[] dataProviderTest1() {
+        return new Object[][]{
+                {"Testing iFrame"},
+                {"Hello!!!"}
+        };
+    }
+
     @Test()
-    public void workWithIFrame() {
+    @UseDataProvider("dataProviderTest1")
+    public void workWithIFrame(String newText) {
         iFramePageSteps.openIFramePage();
         iFramePageSteps.checkIFramePageIsOpen();
         iFramePageSteps.switchToFrame();
@@ -17,8 +30,17 @@ public class FramesTests {
         iFramePageSteps.checkTextIsEntered(newText);
     }
 
+    @DataProvider
+    public static Object[] dataProviderTest2() {
+        return new Object[][]{
+                {"Spring is coming"},
+                {"Xo-Xo-Xo"}
+        };
+    }
+
     @Test
-    public void workWithIFrameEditor() {
+    @UseDataProvider("dataProviderTest2")
+    public void workWithIFrameEditor(String newText) {
         iFramePageSteps.openIFramePage();
         iFramePageSteps.checkIFramePageIsOpen();
         iFramePageSteps.switchToFrame();
@@ -28,6 +50,11 @@ public class FramesTests {
         iFramePageSteps.clickOnUndoButton();
         iFramePageSteps.switchToMainFrame();
         iFramePageSteps.checkUndoAction();
-
     }
+
+    @After
+    public void afterTest() {
+        Selenide.closeWebDriver();
+    }
+
 }
--- src/test/java/UploadFilesTests.java
+++ src/test/java/UploadFilesTests.java
@@ -1,18 +1,37 @@
 import PagesSteps.UploadFilesSteps;
+import com.codeborne.selenide.Selenide;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(DataProviderRunner.class)
 public class UploadFilesTests {
-
-    private final String path = "D:\\Automation\\MyProject\\src\\main\\resources\\";
-    private final String fileName = "Test1.txt";
 
     UploadFilesSteps uploadFilesSteps = new UploadFilesSteps();
 
+    @DataProvider
+    public static Object[] dataProvider() {
+        return new Object[][] {
+                {"D:\\Automation\\TheInternetProject\\src\\main\\resources\\","Test1.txt"},
+                {"D:\\Automation\\TheInternetProject\\src\\main\\resources\\","Test2.txt"},
+                {"D:\\Automation\\TheInternetProject\\src\\main\\resources\\","Test3.txt"},
+                {"D:\\Automation\\TheInternetProject\\src\\main\\resources\\","Test4.txt"}
+        };
+    }
+
     @Test
-    public void fileUpload() {
+    @UseDataProvider("dataProvider")
+    public void fileUpload(String path, String fileName) {
         uploadFilesSteps.openUploadFilesPage();
         uploadFilesSteps.checkUploadFilesPageIsOpen();
         uploadFilesSteps.uploadFileFromPath(path, fileName);
         uploadFilesSteps.checkThatFileIsUploaded(fileName);
+    }
+    @After
+    public void afterTest() {
+        Selenide.closeWebDriver();
     }
 }
